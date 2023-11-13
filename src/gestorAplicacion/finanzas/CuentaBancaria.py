4e24e9f94ebf97bf36cde9637fd4ec2f34a7007d
yohanCami <yosanchezm@unal.edu.co>
2023-11-12 20:10:53 -0500

    funcionalidades estaticas

--- src/gestorAplicacion/finanzas/CuentaBancaria.py
+++ src/gestorAplicacion/finanzas/CuentaBancaria.py
@@ -1,13 +1,60 @@
-class CuentaBancaria():
-    numeroCuentas = 1
-    def __init__(self, saldo, banco):
+from datetime import datetime
 
-        self.numero = CuentaBancaria.numeroCuentas
+from ..usuarios.Empleado import Empleado
+from ..usuarios.Usuario import Usuario
+from ..hotel.Hotel import Hotel
+
+#
+# Clase CuentaBancaria
+#
+#       Hace referencia a la cuenta bancaria de un usuario o un hotel.
+#       Dentro de ella se realizan acciones relacionadas con la cuenta 
+#       bancaria, transacciones, consultas etc.+
+#
+#@autor: Camilo Sánchez
+#
+
+class CuentaBancaria():
+    nCuentas = 1
+    def __init__(self, banco, saldo, numero=None):
+
+        if(numero == None):
+            self.numero = self.nCuenta
+
+        else:
+            self.numero = numero
+
         self.saldo = saldo
         self.banco = banco
         self.ultimoPago = None
 
-        CuentaBancaria.numeroCuentas += 1
+        CuentaBancaria.nCuentas += 1
+
+    #
+    # Método para transferir dinero desde la cuenta que lo llama 
+    # hasta otra cuenta que se le pasa como paramatro.
+    # El método actualiza la fecha del último pago
+    #
+
+    def transfarencia(self, cuenta, valor):
+        self.retirar(valor)
+        
+        cuenta.depositar(valor)
+        cuenta.setUltimoPago(datetime.now().date())
+    
+    #
+    # Método estatico que transfiere dinero de dos cuentas bancarias que se le
+    # pasan como parametro.
+    # Después actualiza la fecha del último pago
+    #
+
+    @classmethod
+
+    def transferencia(cls, cuenta1, cuenta2, valor):
+
+        cuenta1.retirar(valor)
+        cuenta2.depositar(valor)
+        cuenta2.setUltimoPago(datetime.now().date())
         
     def depositar(self, dinero):
         saldo = int(self.saldo)
@@ -18,3 +65,48 @@ class CuentaBancaria():
         saldo = int(self.saldo)
         saldo -= dinero
         self.saldo = saldo
+
+    #
+    # Método estatico que consulta si un hotel tiene saldo suficiente para hacer un
+    # pago
+    #
+
+    @classmethod
+
+    def puedePagarHotel(cls, hotel, pago):
+
+        if hotel.getCuentaBancaria().getSaldo() >= pago:
+            return True
+        else:
+            return False
+        
+    
+    def getNumero(self):
+        return self.numero
+    
+    def setNumero(self, numero):
+        self.numero = numero
+
+    def getBanco(self):
+        return self.banco
+    
+    def setBanco(self, banco):
+        self.banco = banco
+
+    def getSaldo(self):
+        return self.saldo
+    
+    def getSaldo(self, saldo):
+        self.saldo = saldo
+
+    def getUltimoPago(self):
+        return self.ultimoPago
+    
+    def setUltimoPago(self, ultimoPago):
+        self.ultimoPago = ultimoPago
+
+    def getNCuentas(self):
+        return self.nCuentas
+    
+    def setNCuentas(self, numero):
+        self.nCuentas = numero
--- src/gestorAplicacion/usuarios/Administrador.py
+++ src/gestorAplicacion/usuarios/Administrador.py
@@ -1,4 +1,19 @@
+from datetime import datetime
+
 from .Usuario import Usuario
+from ..hotel.Hotel import Hotel
+from ..hotel.Habitacion import Habitacion
+from ..finanzas.CuentaBancaria import CuentaBancaria
+
+#
+# Clase administrador
+#
+#       La clase administrador hereda de la clase usuario.
+#       Un administrador será un usuario con más permisos
+#       dentro del programa
+#
+#@autor: Camilo Sánchez
+#
 
 class Administrador(Usuario):
 
@@ -7,11 +22,145 @@ class Administrador(Usuario):
     def __init__(self, nombre, telefono, usuario, password, cuentaBancaria, hotel):
 
         super().__init__(nombre, telefono, usuario, password, cuentaBancaria)
+        
         self.hotel = hotel
-        self. salario = 500
+        self.salario = 500
         self.ultimoPago = None
         Administrador.totalAdministradores += 1
+
+    def crearAdministrador(self, nombre, telefono, usuario, password, cuentaBancaria, hotel):
+
+        administradorCreado = Administrador(nombre, telefono, usuario, password, cuentaBancaria, hotel)
+
+        return administradorCreado
+    
+    def premiarUsuario():
+        pass
+
+    def hacerDescuento():
+        pass
+
+    def cobrarZonas():
+        pass
+
+    def agregarServicio():
+        pass
+
+    def alterarServicio():
+        pass
+
+    def calcularPromedio():
+        pass
+
+    #Crea una habitación dependiendo el TipoHabitacion (Enum)
+
+    def administradorAgregaHabitacion(self, tipoEscogido):
+        pass
+
+    def listarAdministradores(self):
+
+        administradores = ""
+        contador = 1
+
+        for administrador in self.hotel.getAdministradores():
+            administradores += "    " + str(contador) + ". " + administrador
+            contador += 1
+
+        return administradores
+    
+    def ultimoMesPago(self):
+        return self.getCuentaBancaria().getUltimoPago()
+    
+    def dineroCuentaHotel(self):
+        return self.hotel.getCuentaBancaria().getSaldo()
+    
+    #
+    # Devuelve true si el hotel tiene el suficiente dinero en cuenta
+    # para pagarle a sus empleados y administradores. Llama al método de
+    # la cuenta banacaria para hacer la consulta
+    #
+
+    def puedePagarHotel(self, empleados):
+
+        saldoAPagar = 0
+
+        for empleado in empleados:
+            saldoAPagar += empleado.getSalario()
+
+        saldoAPagar += self.salario
+
+        return CuentaBancaria.puedePagarHotel(self.hotel, saldoAPagar)
+    
+    #
+    # Primero consulta si el último pago se realizó hace más de 30 días,
+    # después consulta si el hotel tiene el dinero suficiente en cuenta
+    # para pagarle a los empleados, si las dos condiciones se cumplen le realiza el pago
+    # a los empleados
+    #
+
+    def pagarEmpleados(self):
+
+        empleados = self.hotel.getEmpleados()
+
+        ultimoPago = self.getUltimoPago()
+        actual = datetime.now().date()
+
+        diferienciaDias = 0
+
+        for empleado in empleados:
+
+            if(empleado.getUltimoPago() == None) or (self.getUltimoPago == None):
+                
+                diferienciaDias = 40
+
+            else:
+
+                diferienciaDias = (actual - ultimoPago).days
+
+        if diferienciaDias < 30:
+            return "Ya se realizó un pago dentro de los últimos 30 días, espera que se cumpla el tiempo para volver a realizarlo"
         
+        else:
+
+            if (self.puedePagarHotel(empleados)):
+
+                for empleado in empleados:
+                    CuentaBancaria.transfarencia(self.hotel.getCuentaBancaria, empleado.getCuentaBancaria, empleado.getSalario)
+                    empleado.setUltimoPago(actual)
+
+                CuentaBancaria.transfarencia(self.getHotel().getCuentaBancaria(), self.getCuentaBancaria(), self.getSalario())
+                self.setUltimoPago(actual)
+
+                return "El pago a los empleados ha sido exitoso"
+            
+            else:
+                return "Saldo en cuenta de hotel insuficiente para hacer el pago"
+
+
+    def getTotalAdministradores(self):
+        return self.totalAdministradores
+    
+    def getHotel(self):
+        return self.hotel
+    
+    def setHotel(self, hotel):
+        self.hotel = hotel
+
+    def getSalario(self):
+        return self.salario
+    
+    def setSalario(self, salario):
+        self.salario = salario
+
+    def aumentoSalario(self, aumento):
+        self.salario += aumento
+
+    def getUltimoPago(self):
+        return self.ultimoPago
+    
+    def setUltimoPago(self, ultimoPago):
+        self.ultimoPago = ultimoPago
+
     def presentacion(self):
         return ""
 
