b18dc3f21e477c4f76f31e928c790577671cfbdd
drestrep <drestrepoag@unal.edu.co>
2023-11-19 14:35:38 -0500

    Cambios de embellecimiento func

--- src/baseDatos/Base.py
+++ src/baseDatos/Base.py
@@ -1,4 +1,8 @@
 from abc import ABC, abstractclassmethod
+import tkinter as tk
+from tkinter import messagebox
+
+from errores.ErrorValores import ErrorValores
 
 #@autor: David Restrepo
 class Base(ABC):
@@ -56,7 +60,7 @@ class Base(ABC):
         cls._empleados.append(empleado)
         
     @classmethod
-    def filtrarPorNombre(cls, nombre):
+    def filtrarPorNombre(cls, nombre, resultados):
         foundHotel = False
         nom = nombre[0]
         for hotel in cls._hoteles:
@@ -65,8 +69,12 @@ class Base(ABC):
                 foundHotel = True
         if foundHotel:
             hotels = [hotelNombre]
+            resultados.delete("1.0", tk.END)
             return hotels
         else:
+            resultados.delete("1.0", tk.END)            
+            resultados.insert(tk.END, "El nombre del hotel que ingresó no se encuentra en la base de datos, intente de nuevo", "centrado")
+            resultados.tag_configure("centrado", justify="center")
             print("El nombre del hotel que ingresó no se encuentra en la base de datos, intente de nuevo: ", end="")
             return None
             
@@ -74,7 +82,7 @@ class Base(ABC):
         
                 
     @classmethod
-    def filtrarPorCiudad(cls, ciudad):
+    def filtrarPorCiudad(cls, ciudad, resultados):
         
         print(ciudad)
         foundHotel = False
@@ -86,32 +94,48 @@ class Base(ABC):
                 hotels.append(hotelNombre)
                 foundHotel = True
         if foundHotel:
+            resultados.delete("1.0", tk.END)  
             return hotels
         else:
+            resultados.delete("1.0", tk.END)            
+            resultados.insert(tk.END, "La ciudad del hotel que ingresó no se encuentra en la base de datos, intente de nuevo", "centrado")
+            resultados.tag_configure("centrado", justify="center")
             print("La ciudad del hotel que ingresó no se encuentra en la base de datos, intente de nuevo: ", end="")
             return None
         
     
     @classmethod
-    def filtrarPorId(cls, identificacion, hotel, huesped):
-        foundRoom = False
+    def filtrarPorId(cls, identificacion, hotel, huesped, resultados):
+        foundHotel = False
         id = identificacion[0]
         habitaciones = hotel.getHabitaciones()
-        for hab in habitaciones:
-            if int(id) == hab.getId():
-                tipoHab = hab.getTipo()
-                if "vip" in tipoHab and not huesped.isVip():
-                    print("No puede acceder a esta habitación, pues no es VIP")
-                    foundHotel = False
-                else:    
-                    habitacion = hab
-                    foundHotel = True
-        if foundHotel:
-            habs = [habitacion]
-            return habs
-        else:
-            print("El Id que ingresó no se encuentra en la base de datos, intente de nuevo: ", end="")
-            return None
+        try:
+            id = int(id)
+            for hab in habitaciones:
+                if int(id) == hab.getId():
+                    tipoHab = hab.getTipo()
+                    if "vip" in tipoHab and not huesped.isVip():
+                        print("No puede acceder a esta habitación, pues no es VIP")
+                        foundHotel = False
+                    else:    
+                        habitacion = hab
+                        foundHotel = True
+            if foundHotel:
+                habs = [habitacion]
+                resultados.delete("1.0", tk.END)    
+                return habs
+            else:
+                resultados.delete("1.0", tk.END)            
+                resultados.insert(tk.END, "El Id que ingresó no se encuentra en la base de datos, intente de nuevo", "centrado")
+                resultados.tag_configure("centrado", justify="center")
+                print("El Id que ingresó no se encuentra en la base de datos, intente de nuevo: ", end="")
+                return None
+        except ValueError:
+            print("Error: No se pudo convertir el string a entero")
+            er = ErrorValores("Id", "Entero")
+            text = er.mostrarMensaje()
+            messagebox.showerror("Error", text)    
+        
         
     @classmethod
     def filtrarPorTipo(cls, hotel, tipo):
